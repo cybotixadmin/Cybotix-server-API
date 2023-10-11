ea0ad2deb2edccaa0259e955a54907cfa2680da7
cybotixadmin <135337925+cybotixadmin@users.noreply.github.com>
2023-10-11 13:12:56 +0200

    cleanout of orphan files

--- routes/about.js
+++
@@ -1,5 +0,0 @@
-exports.about = function(req, res){
-	// documentation at http://expressjs.com/api.html#res.render
-	
-res.render('about', { title: 'Express' });
-};
--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-
-/*
- * GET admin
- */
-
-exports.admin = function(req, res){
-	console.log("admin");
-  res.send("admin");
-};
